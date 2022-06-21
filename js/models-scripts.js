20bd6d5e71015706ebac53baf477bafca6e167be
Leandro Schachner <87399673+LeandroSchachner@users.noreply.github.com>
2022-06-21 18:05:07 -0300

    Add files via upload

---
+++ js/models-scripts.js
@@ -0,0 +1,10 @@
+
+
+//MODAL
+const myModal = document.getElementById('modal')
+const myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', () => {
+  myInput.focus()
+  console.log(myModal);
+})
